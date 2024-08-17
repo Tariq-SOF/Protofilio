5603c5ae996c019aa68fe2cc80c634622609bcbd
009661@#m <tari.almutairi1995@gmai.com>
2024-08-17 23:25:10 +0300

    Rename

--- src/components/Rout.jsx
+++ src/components/Rout.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactGA from 'react-ga';
 import Homep from "./Homep";
 import About from "./About";
 import Projects from "./Projects"
 export default function Rout() {
+
+  ReactGA.initialize('G-X6JCGSSLX3', {
+  gaOptions: {
+    userId: '8572916208',
+    customDimensions: {
+    dimension1: 'premium'
+    }
+  }
+})
+
+const Viewuser = (location) => {
+  ReactGA.set({ page: location.pathname }); 
+  ReactGA.pageview(location.pathname); 
+};
+
   return (<>
   <Routes>
-    <Route path="/" element = {<Homep/>}/>
-     <Route path="/Homep" element = {<Homep/>}/>
-    <Route path="/" element = {<sl/>}/>
-    <Route path="/About" element = {<About/>}/>
-    <Route path="/Projects" element = {<Projects/>}/>
-    
-
-    
+    <Route path="/" element = {<Homep/>} 
+    render={(props) => {
+            Viewuser(props.location);
+            return <Homep />
+          }}/>
+     <Route path="/Homep" element = {<Homep/>}
+     render={(props) => {
+            Viewuser(props.location);
+            return <Homep />
+          }}/>
+    <Route path="/About" element = {<About/>}
+    render={(props) => {
+            Viewuser(props.location);
+            return <About />
+          }}/>
+    <Route path="/Projects" element = {<Projects/>}
+    render={(props) => {
+            Viewuser(props.location);
+            return <Projects />
+          }}/>
   </Routes>
 
   </>)
