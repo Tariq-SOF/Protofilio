1eb8398b3b2e12b50ecc425c285971b0692a6f00
009661@#m <tari.almutairi1995@gmai.com>
2024-08-18 12:20:20 +0300

    Rename

--- src/components/Rout.jsx
+++ src/components/Rout.jsx
@@ -1,48 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ReactGA from 'react-ga';
 import Homep from "./Homep";
 import About from "./About";
-import Projects from "./Projects"
+import Projects from "./Projects";
+
 export default function Rout() {
-
   ReactGA.initialize('G-X6JCGSSLX3', {
-  gaOptions: {
-    userId: '8572916208',
-    customDimensions: {
-    dimension1: 'premium'
+    gaOptions: {
+      userId: '8572916208',
+      customDimensions: {
+        dimension1: 'premium'
+      }
     }
-  }
-})
+  });
 
-const Viewuser = (location) => {
-  ReactGA.set({ page: location.pathname }); 
-  ReactGA.pageview(location.pathname); 
-};
+  const Viewuser = () => {
+    const location = useLocation();
+    useEffect(() => {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.pageview(location.pathname);
+    }, [location]);
+  };
 
-  return (<>
-  <Routes>
-    <Route path="/" element = {<Homep/>} 
-    render={(props) => {
-            Viewuser(props.location);
-            return <Homep />
-          }}/>
-     <Route path="/Homep" element = {<Homep/>}
-     render={(props) => {
-            Viewuser(props.location);
-            return <Homep />
-          }}/>
-    <Route path="/About" element = {<About/>}
-    render={(props) => {
-            Viewuser(props.location);
-            return <About />
-          }}/>
-    <Route path="/Projects" element = {<Projects/>}
-    render={(props) => {
-            Viewuser(props.location);
-            return <Projects />
-          }}/>
-  </Routes>
-
-  </>)
+  return (
+    <>
+      <Viewuser />
+      <Routes>
+        <Route path="/" element={<Homep />} />
+        <Route path="/Homep" element={<Homep />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </>
+  );
 }
